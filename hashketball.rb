c01e1e30491b8169683b0df3bb348843a8ab910e
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-05 16:28:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
-require 'csv' 
+require 'csv'
 data = CSV.parse(<<~ROWS, headers: true)
 
 |        Stat        |     Info          |         Info      |       Info        |         Info    |         Info      |
