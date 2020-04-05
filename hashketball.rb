67333073ee88fedd1ade861a19b215cb610b9c7f
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-05 20:29:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -51,8 +51,8 @@ end
 if !new_hash.has_key?(:away)
   new_hash[:away] =  {
       :team_name => new_array[20],
-      :colors => new_array[21]
-      :players => new_array[26] 
+      :colors => new_array[21],
+      :players => new_array[26]
       }
 end
 
