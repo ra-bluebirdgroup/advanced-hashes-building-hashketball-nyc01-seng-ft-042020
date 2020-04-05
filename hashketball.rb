3c2c225f4e56b1305287c8e229627039e746eed5
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-05 16:30:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,6 +13,7 @@ data = CSV.parse(<<~ROWS, headers: true)
 | **Slam Dunks**     | 2                 | 10                | 5                 | 0               | 12                |
 ROWS
 
+puts data.first
 
 
 # def game_hash(table)
