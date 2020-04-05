b04e860e90af987c58b3c6ca3ac660110b27367b
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-05 18:57:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -37,7 +37,7 @@ Away Team:
 | **Slam Dunks**     | 2                 | 10                | 5                 | 0               | 12                |
 
 ROWS
-new_array = data.to_h
+new_hash = data.to_h
 
  p new_array
 
